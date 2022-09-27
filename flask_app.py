d33ec58cca41f181dd29172d4f9ceac783a701d4
Tomy <tlugaro@agro.uba.ar>
2022-09-27 18:45:38 -0300

    update

--- flask_app.py
+++ flask_app.py
@@ -1,25 +1,15 @@
 from flask import Flask, render_template, url_for
-from flask_wtf import FlaskForm
-from flask import send_file
-import folium
-from folium import plugins
+import requests
 import urllib
 import json
-from urllib import request
-import requests
 from requests.auth import AuthBase
 from Crypto.Hash import HMAC
 from Crypto.Hash import SHA256
 from datetime import datetime, timedelta
 from dateutil.tz import tzlocal
 from pandas import json_normalize
-import pandas as pd
-from osgeo import gdal
-from osgeo import ogr
 import numpy as np
 import pandas as pd
-import geopandas
-import psycopg2
 import math
 
 app = Flask(__name__)
@@ -59,7 +49,7 @@ def home_page():
 
     # Service/Route that you wish to call
     apiRoute = '/data/0020B01A/hourly/last/1h'
-    apiRoute2 = '/data/0020B01A/hourly/last/24h'
+    apiRoute2 = '/data/0020B01A/hourly/last/48h'
 
     auth = AuthHmacMetosGet(apiRoute, publicKey, privateKey)
     response = requests.get(apiURI + apiRoute, headers={'Accept': 'application/json'}, auth=auth)
@@ -80,9 +70,9 @@ def home_page():
     valoresmax = df2['values.max']
     ppt=valoressum[3]
 
-    pp= round((sum(ppt)),2)
-    tmax= max(valoresmax[6])
-    tmin=min(valoresmin[6])
+    pp= round((sum(ppt)),1)
+    tmax= round(max(valoresmax[6]),1)
+    tmin=round(min(valoresmin[6]),1)
 
     marchaviento = valores2[11]
     marchadirviento = valores2[12]
@@ -97,24 +87,22 @@ def home_page():
     nombres = df['name'].tolist()
     valores = df['values.avg']
 
-    wind=  str(valores[11][0])
-    winddir = str(valores[12])
-    eto = str(valores[22]) + " " + str(unidades[22])
-    radiacion =str(valores[0][0]) + " " + str(unidades[0])
-    temp_suelo = str(valores[1][0]) + " " + str(unidades[1])
-    temp_air =str(valores[6][0]) + " " + str(unidades[6])
-    humedad = str(valores[7][0]) + " " + str(unidades[7])
-    now= datetime.now()
-    hour= str(now.hour)+":"+str(now.minute)
+    wind= "Sensor no responde" if str(valores[11])=="nan" else str(valores[11][0])+ " km/h"
+
+
+    radiacion =str(round(valores[0][0],1)) + " " + str(unidades[0])
+    temp_suelo = str(round(valores[1][0],1)) + " " + str(unidades[1])
+    temp_air =str(round(valores[6][0],1)) + " " + str(unidades[6])
+    humedad = str(round(valores[7][0],1)) + " " + str(unidades[7])
+    now= datetime.now()- timedelta(hours=3)
+    hour= str(now.hour)
+    hour1=str((datetime.now()- timedelta(hours=2)).hour)
     date = str(now.day) + "/" + str(now.month)
-    hours=[23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
+    hours=[47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
     d=[]
     for i in hours:
-        d.append(((datetime.now() - timedelta(hours=i)).hour))
+        d.append(((datetime.now() - timedelta(hours=3 )- timedelta(hours=i)).hour))
 
-    return render_template('ema.html',d=d,windir=winddir,wind=wind,puntorocio=marchapuntorocio,eto=eto,mrad=marcharad,mhum=marchahum,mpp=ppt,mtsuelo=marchatsuelo,mtmin=marchatmin,mtmed=marchatmed,mtmax=marchatmax,tmax=tmax,tmin=tmin, rad=radiacion,pp=pp, temp= temp_suelo, temp2=temp_air, hum=humedad,hora=hour,date=date)
+    return render_template('ema.html',d=d,hour1=hour1,wind=wind,puntorocio=marchapuntorocio,mrad=marcharad,mhum=marchahum,mpp=ppt,mtsuelo=marchatsuelo,mtmed=marchatmed,tmax=tmax,tmin=tmin, rad=radiacion,pp=pp, temp= temp_suelo, temp2=temp_air, hum=humedad,hora=hour,date=date)
 
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
